18bcffb61d1b8b8c034d7504f276c8581128cfe7
osmiwol <osmiwol@gmail.com>
2023-06-28 15:52:48 +0500

    Исправил баг с задержкой после отпускания кнопки

--- Assets/Code/Player/PlayerController.cs
+++ Assets/Code/Player/PlayerController.cs
@@ -25,7 +25,7 @@ public class PlayerController : MonoBehaviour
     float _halfScreenWith;
     
     bool _canJump = true;
-    
+    string _nameCurrentScene = "";
 
     // Start is called before the first frame update
     void Start()
@@ -33,49 +33,54 @@ public class PlayerController : MonoBehaviour
         _rb = GetComponent<Rigidbody2D>();        
         ResetVariables();
         _halfScreenWith = Screen.width / 2;
-        
+
+        _nameCurrentScene = SceneManager.GetActiveScene().name;
     }
     
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y < -15) SceneManager.LoadScene("SampleScene");
-        if (!_canJump) return;
+        CheckPosition();
         _jumpDirection = GetJumpDirection();
+        Debug.Log($"Direcion: {_jumpDirection}");
+        Vector2 transPos = transform.position;
 
+        if (_jumpDirection != 0)
+        {
+            _offset = 1f * _jumpDirection;
+            DirectionShower.transform.position = new Vector2(transPos.x + (0.3f * _offset), transPos.y + 0.65f);
+        }
+
+        if (!_canJump) return;
+         
         if (_jumpDirection == 0 || JumpPower >= MaxJumpPower)
         {
             if (JumpPower == 0) return;
             else 
             {
+                JumpTarget.transform.position = new Vector2(transPos.x + _offset, transPos.y + 1.5f);
+
                 Vector2 jumpVector = JumpTarget.transform.position - transform.position;
                 
                 Debug.Log($"jmpVec:{jumpVector} pwr:{JumpPower}");
                 _rb.AddForce(jumpVector * JumpPower, ForceMode2D.Impulse);
+                
                 ResetVariables();
             }
         }
         else
-        {            
-            Vector2 transPos = transform.position;
-
-            if (_jumpDirection > 0) _offset = 1f;
-            else if (_jumpDirection < 0) _offset = -1f;
-
-            JumpTarget.transform.position = new Vector2(transPos.x + _offset, transPos.y + 1.5f);
-            DirectionShower.transform.position = new Vector2(transPos.x + (0.3f * _offset), transPos.y + 0.65f);
-            
-
+        {
             if (JumpPower < MaxJumpPower)
             {
                 float incValue = JumpIncreaser * Time.deltaTime;
                 JumpPower += incValue;
+                
                 currentRedColor += incValue / MaxJumpPower;
                 Debug.Log($" inc: {incValue} jp:{JumpPower} redCol:{currentRedColor}");
                 spriteRendDirectionShower.color = spriteRendPlayer.color = new Color(1, 1-currentRedColor, 1-currentRedColor, 1 );
-            }
-            
+            }            
         }
+        
     }
 
     float direction = 0;
@@ -90,14 +95,19 @@ public class PlayerController : MonoBehaviour
                 if (touches[i].phase == TouchPhase.Began)
                 {
                     touch = touches[i];
-                    Debug.Log(touch.position);
                     if (touch.position.x > _halfScreenWith) direction = 1;
                     else direction = -1;
                 }
             }
         }
-        else direction = Input.GetAxis("Horizontal");
-
+        else
+        {
+            direction = Input.GetAxis("Horizontal");
+            if (direction < -0.3) direction = -1;
+            else if (direction > 0.3) direction = 1;
+            else direction = 0;
+        }
+        
         return direction;
     }
 
@@ -106,7 +116,7 @@ public class PlayerController : MonoBehaviour
         JumpPower = 0;
         _jumpDirection = 0;
         DirectionShower.transform.position  = JumpTarget.transform.position = transform.position;
-        spriteRendDirectionShower.color = spriteRendPlayer.color = new Color(1, 1, 1, 1);//
+        spriteRendDirectionShower.color = spriteRendPlayer.color = new Color(1, 1, 1, 1);
          
         currentRedColor = 0;
     }
@@ -124,5 +134,10 @@ public class PlayerController : MonoBehaviour
     private void OnCollisionExit2D(Collision2D collision)
     {
         if(collision.contactCount == 0) _canJump = false;
+    }
+
+    void CheckPosition()
+    {
+        if (transform.position.y < -15) SceneManager.LoadScene(_nameCurrentScene);
     }
 }
